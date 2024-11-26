baf0a321f952436c3f7d8d8c95ac1213972b21f0
yezhangchao <ye.zhangchao@seatell.cn>
2024-11-26 11:39:03 +0800

    del

--- .history/src/app/reliability/page_20241122161846.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Reliability() {
-  return (
-    <div>page</div>
-  )
-}
--- .history/src/app/reliability/page_20241122161903.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Reliability() {
-  return (
-    <div>Reliability</div>
-  )
-}
